44e566c76f1634d485640061b609a0fb2ba59725
Tun Myat <tunmyat.bryan@gmail.com>
2020-10-30 15:21:53 -0700

    Deep Learning

---
+++ testing.py
@@ -0,0 +1,36 @@
+# Larger CNN for the MNIST Dataset
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+
+import os
+
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# reshape to be [samples][width][height][channels]
+X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+# load model
+model = load_model('model.h5')
+# summarize model
+model.summary()
+# load dataset
+# evaluate the model
+score = model.evaluate(X_test, y_test, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+
+os.system("pause")
